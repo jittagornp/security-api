b5f37ca7803a278340d14c8211044358880b6273
jittagorn pitakmetagoon <jittagornp@gmail.com>
2016-04-27 07:27:24 +0700

    Create AuthenticationException.java

---
+++ src/main/java/com/pamarin/security/api/exception/AuthenticationException.java
@@ -0,0 +1,16 @@
+/*
+ *  Copyright 2016 Pamarin.com
+ */
+package com.pamarin.security.api.exception;
+
+/**
+ *
+ * @author jittagornp
+ */
+public class AuthenticationException extends RuntimeException {
+
+    public AuthenticationException(String message) {
+        super(message);
+    }
+
+}
